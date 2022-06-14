31fde69ec667846d17d453b9bca2570202f2251f
Ceckmateeeee21 <37415742+Ceckmateeeee21@users.noreply.github.com>
2022-06-14 23:53:25 +0800

    back to top

---
+++ test.js
@@ -0,0 +1,22 @@
+//記得放在vs cod 要用<script>包起來
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    /*不知道為什麼歸零的時候，沒消失back to top btn */
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
